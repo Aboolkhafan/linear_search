e5f3743232e07d7a4c704803d62570ae0d925056
Aboolkhafan <85876607+Aboolkhafan@users.noreply.github.com>
2021-06-22 11:58:29 +0430

    Linear Search Algorithm
    Source code of linear search algorithm

---
+++ linear_search.py
@@ -0,0 +1,13 @@
+"""
+    linear(sequential) search
+        [1, 2, 5, 8, 11, 14, 23, 32, 48], 11  =>  4
+"""
+
+def linear_search(array, element):
+    for i in range(len(array)):
+        if array[i] == element:
+            return i
+
+    return None
+
+print(linear_search([1, 2, 5, 8, 11, 14, 23, 32, 48], 11))
